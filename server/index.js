86d9833c0772d98bde38236641d12c2793ca7094
bishopwalker <walkerbishop333@gmail.com>
2020-08-14 11:19:50 -0700

    server

--- server/index.js
+++ server/index.js
@@ -2,5 +2,10 @@ require('dotenv').config()
 const express = require('express'),
 awsRouter = require('./controllers/awsRouter'), 
 session = require('express-session'),
+app = express();
 
 app.use(express.json())
+
+app.use(awsRouter);
+
+app.listen(3333, () =>console.log('Listening to port 3333'))
