5cfd121e9b38a7121a7533619d753b5949e3bf07
bishopwalker <walkerbishop333@gmail.com>
2020-08-17 09:47:27 -0700

    carosel

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,11 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import Carousel from 'react-simply-carousel'
 
-const Carousel = () => {
+const MyCarousel = () => {
+    const [activeSlide, setActiveSlide] = useState(0)
     return (
         <div>
-            
+            <Carousel
+                containerProps={{
+                    style: {
+                        width: '100%',
+                        justifyContent: 'space-between'
+                    }
+                }}
+                activeSlideIndex={activeSlide}
+                activeSlideProps={{
+                    style: {
+                        background: 'blue'
+                    }
+                }}
+                onRequestChange={setActiveSlide}
+                forwardBtnProps={{
+                    children: '>',
+                    style: {
+                        width: 30,
+                        height: 30,
+                        minWidth: 30,
+                        alignSelf: 'center'
+                    }
+                }}
+                backwardBtnProps={{
+                    children: '<',
+                    style: {
+                        width: 30,
+                        height: 30,
+                        minWidth: 30,
+                        alignSelf: 'center'
+
+                    }
+                }}
+
+                itemsToShow={7}
+                speed={400}
+            >
+                {Array.from({ length: 10 }).map((item, index) => (
+                    <div
+                        style={{
+                            background: "red",
+                            width: 150,
+                            height: 150,
+                            border: "30px solid white",
+                            textAlign: "center",
+                            lineHeight: "240px",
+                            boxSizing: "border-box"
+                        }}
+                        key={index}
+                    >
+                        {index}
+                    </div>
+                ))}
+
+            </Carousel>
         </div>
     )
 }
 
-export default Carousel
+export default MyCarousel
