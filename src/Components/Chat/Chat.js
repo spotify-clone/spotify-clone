c08d5c6ec6e17d4e308bab961ec7562accfc562c
bishopwalker <walkerbishop333@gmail.com>
2020-08-18 10:54:34 -0700

    Chat Sick with it

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -7,37 +7,84 @@ import './Chat.css'
 
 let socket;
 
-function Chat({location}) {
-  
-  const [name, setName] = useState('');
-  const [room, setRoom] = useState('');
-  const [message, setMessage] = useState('');
-const ENDPOINT = 'localhost:3333'
-   
-useEffect(()=>{
-        const {name,room} = queryString.parse(location.search)
-socket = io.connect();
-      setName(name)
-      setRoom(room)
+function Chat({ location }) {
 
-      console.log(socket)
-socket.on('message', message =>setMessage(draft => {
-    draft.push(['', message])
-}))
-      // socket.emit('join', {name, room})
-    },[ENDPOINT,location.search])
-   
-      
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const [message, setMessage] = useState('');
+    const [receivedMessages, setReceivedMessages] = useState([])
+    const ENDPOINT = 'localhost:3333'
 
+
+
+    //First end point connecting the socket to the end point
+    useEffect(() => {
+        socket = io.connect(ENDPOINT)
+        const { name, room } = queryString.parse(location.search)
+         setName(name)
+        setRoom(room)
+
+        console.log(socket)
+
+     }, [location.search])
+
+
+//Receiving the message from the server and then setting it on state
+    useEffect(() => {
+        socket.on('message from server', message => {
+            console.log(message)
+ //This is the same as saying prev state =>prev state using a component
+ 
+            setReceivedMessages(receivedMessages => [...receivedMessages, message])
+
+        })
+    }, [])
+
+
+
+
+    const sendMessage = (event) => {
+        console.log('hit send message', message)
+        if (message) {
+            socket.emit('message', { message, name })
+        }
+    }
+
+//Mapping the returned messages from the server the display
+    const mappedMessages = receivedMessages.map((word, index) => {
+        return (
+            <div key={index} >
+            <span>{word.name} says: </span>
+                <span> { word.message}</span>
+            </div>
+        )
+    })
+    console.log(name)
+
+    //Trying to create a condition where the name box goes away after the informaion is entered.
+    //Right now since any value makes it truthy it goes away....
     return (
         <div>
-        <h1>Chat</h1>
-        <input 
-            type='text'
-            name='text'
-            onChange={(e)=>e.target.value}
-        />
-        <textarea>{setMessage}</textarea>
+            { name?
+                null :
+                <div>
+                    <span>Enter Your name</span>
+
+                    <input
+                        type='text'
+                        name='name'
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>}
+            <h1>Chat</h1>
+            <input
+                type='text'
+                name='text'
+                onChange={(e) => setMessage(e.target.value)}
+                onKeyPress={event => event.key === 'Enter' ? sendMessage(event, name) : null}
+            />
+            <button onClick={(e) => sendMessage(e)} >Send</button>
+            {mappedMessages}
         </div>
     )
 }
