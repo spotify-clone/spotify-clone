73e7a2f4bccc505d3d155d4f9aedbfef939b64fc
whroach259@gmail.com <whroach259@gmail.com>
2020-08-20 16:53:37 -0500

    search component able to get artist bio and tracks

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -5,34 +5,59 @@ import axios from 'axios'
 
 const Search = (props) => {
     const [artist, setArtist] = useState([])
+    const [tracks, setTracks] = useState([])
+
+    //artist = [ 'drake'] == artist.length  = 1
+
 
     useEffect(()=>{
         searchArtist()
-    },[artist])
+    },[])
     
 
 
     let searchArtist=()=>{
-
         const value = props.match.params.artist
-        console.log(value)
+
         axios.get(`/api/search/?search=${value}`)
-        .then(res => setArtist(res.data))
+        .then(res => {
+            if(res.data.length !== artist.length){
+                setArtist(res.data)
+                res.data.forEach(element=>{
+                    if(element.id){
+                        axios.get(`/api/artist-track/${element.id}`)
+                        .then(res =>{
+                            setTracks(res.data)
+                        })
+
+                    }
+                })
+                // setArtist(res.data)
+            }
+
+        })
         .catch((err)=> console.log(err));
 
     }
+
+
+
+    // const mappedArtists = artist.map((element,index)=>{
+    //     return <img key={index} src={element.images[0].url} />
+    // })
+
+
+
+
+
     
-    console.log(artist.items.name)
+
 
 
 
     return (
-        <div>
-           {/* ( {artist}?
-           <div>
-                    <h1>{artist.items}</h1>
-           </div>
-           :null) */}
+        <div style={{position:"relative", left:"50%"}}>
+            {/* {mappedArtists} */}
         </div>
     )
 }
