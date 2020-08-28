2b33993671d7126de675f10ff8ad8d5493fac0ae
bishopwalker <walkerbishop333@gmail.com>
2020-08-28 07:19:56 -0700

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
-import axios from 'axios;'
+import App, {getAllTracks} from './App';
+import mockAxios from 'axios;'
+import { Item } from 'semantic-ui-react';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('calls axios and returns music', async () => {
+  mockAxios.get.mockImplentationOnce(() =>{
+  Promise.resolve({
+    data: {
+      results: ['something.mp3']
+    }
+  })
+  })
+  expect(mockAxios.get).toHaveBeenCalledTimes(1);
+})
